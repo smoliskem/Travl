f54859ec4f45ec352b5f59480e96677232b0b7c1
Даниил Никитин <05092005daniil@mail.ru>
2024-11-18 19:34:57 +0300

    Сверстал страницу регистрации

--- app/src/main/java/com/example/travl/MainActivity.kt
+++ app/src/main/java/com/example/travl/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.welcome_page)
+        setContentView(R.layout.sign_in_page)
 
 
     }
