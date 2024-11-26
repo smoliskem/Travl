9e96935fc8605413dcba63fe449320d98f836ea5
smoliskem <pogromartem@ya.ru>
2024-11-26 23:51:00 +0300

    navigation preset, place_card preset

--- app/src/main/java/com/example/travl/MainActivity.kt
+++ app/src/main/java/com/example/travl/MainActivity.kt
@@ -2,20 +2,17 @@ package com.example.travl
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.travl.ui.theme.TravlTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.welcome_page)
+        setContentView(R.layout.activity_main)
     }
+
+
 }
---
+++ app/src/main/java/com/example/travl/pages/WelcomePage.kt
@@ -0,0 +1,31 @@
+package com.example.travl.pages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.travl.R
+
+
+class WelcomePage : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.welcome_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val signIn = view.findViewById<Button>(R.id.signInBtn)
+        val signUp = view.findViewById<Button>(R.id.signUpBtn)
+        val controller = findNavController()
+        signIn.setOnClickListener { controller.navigate(R.id.place_page) }
+        signUp.setOnClickListener { controller.navigate(R.id.place_page) }
+    }
+}
