1ad2437473f98c888a6f55ccc95046ac518e2b84
Даниил Никитин <05092005daniil@mail.ru>
2025-02-13 18:59:41 +0300

    moved RecyclerView to a fragment

--- app/src/main/java/com/example/travl/pages/MainPage.kt
+++ app/src/main/java/com/example/travl/pages/MainPage.kt
@@ -6,12 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.travl.R
+import com.example.travl.adapters.MainPageParentItemAdapter
 import com.example.travl.databinding.MainPageBinding
+import com.example.travl.items.MainPageItemGenerator
+import com.example.travl.items.MainPageParentItem
 
 
 class MainPage : Fragment() {
     private lateinit var binding: MainPageBinding
+    private lateinit var adapter: MainPageParentItemAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,26 @@ class MainPage : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = MainPageBinding.bind(view)
+
+
+        val innerItems1 = MainPageItemGenerator.generateMainPageItem(5)
+        val innerItems2 = MainPageItemGenerator.generateMainPageItem(8)
+        val innerItems3 = MainPageItemGenerator.generateMainPageItem(3)
+        val outerItem = listOf(
+            MainPageParentItem(innerItems2, "Регион 1"),
+            MainPageParentItem(innerItems1, "Регион 2"),
+            MainPageParentItem(innerItems3, "Регион 3")
+        )
+
+        adapter = MainPageParentItemAdapter()
+        adapter.data = outerItem
+
+        val manager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+
+
+        binding.mainPageRecycler.layoutManager =
+            manager // Назначение LayoutManager для RecyclerView
+        binding.mainPageRecycler.adapter = adapter // Назначение адаптера для RecyclerView
 
         binding.myPlansBtn.setOnClickListener {
             findNavController().navigate(MainPageDirections.actionMainPageToMyPlansPage())
