0cc7e15df10fe585927d98894143e4e655cd240c
Popiron23 <galushko.ant@yandex.ru>
2025-02-13 21:47:01 +0300

    refactor MyPlansPage

--- app/src/main/java/com/example/travl/pages/MyPlansPage.kt
+++ app/src/main/java/com/example/travl/pages/MyPlansPage.kt
@@ -1,20 +1,19 @@
 package com.example.travl.pages
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.travl.R
+import com.example.travl.databinding.MyPlansPageBinding
 
 
 class MyPlansPage : Fragment() {
-
+    private lateinit var binding: MyPlansPageBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.my_plans_page, container, false)
@@ -22,13 +21,17 @@ class MyPlansPage : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mainBtn = view.findViewById<ImageButton>(R.id.main_page_btn)
-        val withBtn = view.findViewById<ImageButton>(R.id.joint_plans_btn)
-        val profileBtn = view.findViewById<ImageButton>(R.id.profile_btn)
+        binding = MyPlansPageBinding.bind(view)
         val controller = findNavController()
 
-        mainBtn.setOnClickListener { controller.navigate(MyPlansPageDirections.actionMyPlansPageToMainPage()) }
-        withBtn.setOnClickListener { controller.navigate(MyPlansPageDirections.actionMyPlansPageToJointPlansPage()) }
-        profileBtn.setOnClickListener { controller.navigate(MyPlansPageDirections.actionMyPlansPageToProfilePage()) }
+        binding.mainPageBtn.setOnClickListener {
+            controller.navigate(MyPlansPageDirections.actionMyPlansPageToMainPage())
+        }
+        binding.jointPlansBtn.setOnClickListener {
+            controller.navigate(MyPlansPageDirections.actionMyPlansPageToJointPlansPage())
+        }
+        binding.profileBtn.setOnClickListener {
+            controller.navigate(MyPlansPageDirections.actionMyPlansPageToProfilePage())
+        }
     }
 }
