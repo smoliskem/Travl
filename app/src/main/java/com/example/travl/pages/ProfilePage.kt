9ca57ffdd55ef9be0e9a5c25de8f9280cbb26ded
Даниил Никитин <05092005daniil@mail.ru>
2025-03-20 19:32:24 +0300

    added username saving

--- app/src/main/java/com/example/travl/pages/ProfilePage.kt
+++ app/src/main/java/com/example/travl/pages/ProfilePage.kt
@@ -1,11 +1,13 @@
 package com.example.travl.pages
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.travl.R
 import com.example.travl.databinding.MainPageBinding
@@ -14,13 +16,22 @@ import com.example.travl.databinding.ProfilePageBinding
 
 class ProfilePage : Fragment() {
     private lateinit var binding: ProfilePageBinding
+    private lateinit var textViewUserName: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.profile_page, container, false)
+
+        textViewUserName = view.findViewById(R.id.username)
+
+        val sharedPreferences = requireContext().getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val userName = sharedPreferences.getString("user_name", "Default Name")
+
+        textViewUserName.text = userName
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_page, container, false)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
