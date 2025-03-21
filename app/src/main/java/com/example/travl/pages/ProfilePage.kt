ce527aa74a165bfe4a455de956ec67011139b688
Даниил Никитин <05092005daniil@mail.ru>
2025-03-21 12:47:49 +0300

    add button for log out and implement this

--- app/src/main/java/com/example/travl/pages/ProfilePage.kt
+++ app/src/main/java/com/example/travl/pages/ProfilePage.kt
@@ -1,6 +1,7 @@
 package com.example.travl.pages
 
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,19 +10,24 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.example.travl.LoginActivity
 import com.example.travl.R
 import com.example.travl.databinding.MainPageBinding
 import com.example.travl.databinding.ProfilePageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ProfilePage : Fragment() {
     private lateinit var binding: ProfilePageBinding
     private lateinit var textViewUserName: TextView
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = FirebaseAuth.getInstance()
+
         val view = inflater.inflate(R.layout.profile_page, container, false)
 
         textViewUserName = view.findViewById(R.id.username)
@@ -48,5 +54,17 @@ class ProfilePage : Fragment() {
             findNavController().navigate(ProfilePageDirections.actionProfilePageToJointPlansPage())
         }
 
+        binding.logOut.setOnClickListener {
+            signOut()
+        }
+
+    }
+
+    private fun signOut() {
+        auth.signOut() // Выход из аккаунта
+        // Обновите UI или перенаправьте пользователя на экран входа
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
     }
 }
