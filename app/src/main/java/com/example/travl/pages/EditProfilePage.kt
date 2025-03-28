750408fef979a2f689411cc0ac2f245bb1b922e1
Даниил Никитин <05092005daniil@mail.ru>
2025-03-28 14:34:39 +0300

    Added username editing

--- app/src/main/java/com/example/travl/pages/EditProfilePage.kt
+++ app/src/main/java/com/example/travl/pages/EditProfilePage.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.travl.R
 import com.example.travl.databinding.EditProfilePageBinding
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class EditProfilePage : Fragment() {
     private lateinit var binding: EditProfilePageBinding
+
     //private lateinit var textViewUserName: TextView
     private lateinit var auth: FirebaseAuth
+    private lateinit var user: FirebaseUser
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,12 +35,7 @@ class EditProfilePage : Fragment() {
         val view = inflater.inflate(R.layout.edit_profile_page, container, false)
 
         auth = Firebase.auth
-        //textViewUserName = view.findViewById(R.id.username)
 
-        //val user = FirebaseAuth.getInstance().currentUser
-        //val displayName = user?.displayName
-
-        //textViewUserName.text = displayName
         // Inflate the layout for this fragment
         return view
     }
@@ -40,10 +44,48 @@ class EditProfilePage : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = EditProfilePageBinding.bind(view)
 
+
         binding.backBtn.setOnClickListener {
             findNavController().navigate(EditProfilePageDirections.actionEditPageToProfilePage())
         }
 
+        binding.acceptBtn.setOnClickListener {
+            val editName = binding.editName.text.toString()
+            user = auth.currentUser!!
 
+            if (editName.isNotEmpty()) {
+                updateUsername(user, editName)
+            } else {
+                findNavController().navigate(EditProfilePageDirections.actionEditPageToProfilePage())
+            }
+        }
     }
+
+    //Смена имени
+    private fun updateUsername(user: FirebaseUser?, userName: String) {
+        val profileUpdates = UserProfileChangeRequest.Builder()
+            .setDisplayName(userName)
+            .build()
+
+        user?.updateProfile(profileUpdates)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Username saved",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    // Навигация только после успешного обновления
+                    findNavController().navigate(EditProfilePageDirections.actionEditPageToProfilePage())
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Setting username failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+
 }
